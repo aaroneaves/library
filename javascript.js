dd870fac44be38207957d67ae2cc8738634749eb
Aaron <stephenaaroneaves@gmail.com>
2022-12-10 18:12:46 -0500

    Basic form and inputs are setup   Basic styling done

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read ) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const yes = document.querySelector('#yes');
+const no = document.querySelector('#no');
+
+
+
+
+
+function addBookToLibrary() {
+    //do stuff here
+}
+
