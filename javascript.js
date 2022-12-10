cfb148e832e0975973711799da9b33da8465dea2
Aaron <stephenaaroneaves@gmail.com>
2022-12-10 18:57:34 -0500

    JS first steps   Query selectors added   Submit button creates new book and clears all fields

--- javascript.js
+++ javascript.js
@@ -1,5 +1,16 @@
 let myLibrary = [];
 
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const yes = document.querySelector('#yes');
+const no = document.querySelector('#no');
+
+const submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', () => {
+    addBookToLibrary();
+});
+
 function Book(title, author, pages, read ) {
     this.title = title;
     this.author = author;
@@ -7,17 +18,12 @@ function Book(title, author, pages, read ) {
     this.read = read;
 }
 
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const yes = document.querySelector('#yes');
-const no = document.querySelector('#no');
-
-
-
-
-
 function addBookToLibrary() {
-    //do stuff here
+    window.book = new Book(title.value, author.value, pages.value, yes.checked);
+    title.value = null;
+    author.value = null;
+    pages.value = null;
+    yes.checked = false;
+    no.checked = false;
 }
 
