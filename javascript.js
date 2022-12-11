aee82c436d19514fccba1699699107983bae42ed
Aaron <stephenaaroneaves@gmail.com>
2022-12-11 17:51:48 -0500

    New Book and Remove Buttons   New Book button works correctly. Form appears/disappears appropriately   Remove buttons added but don't work properly

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+//Library, Book, and books
+
 let myLibrary = [];
 
 const title = document.querySelector('#title');
@@ -6,11 +8,6 @@ const pages = document.querySelector('#pages');
 const yes = document.querySelector('#yes');
 const no = document.querySelector('#no');
 const bookContainer = document.querySelector('.book-container');
-
-const submitButton = document.querySelector('#submit-button');
-submitButton.addEventListener('click', () => {
-    addBookToLibrary();
-});
 
 function Book(title, author, pages, read ) {
     this.title = title;
@@ -28,16 +25,63 @@ function addBookToLibrary() {
     pages.value = null;
     yes.checked = false;
     no.checked = false;
+    closeTheForm();
 }
 
 function createCard () {
     div = document.createElement("div");
     div.classList.add('box');
-    div.style.height = "100px";
-    div.style.width = "800px";
-    div.style.backgroundColor = '#c0c0c0';
-    div.style.border = "solid 1px #000000";
+    div.setAttribute('data-index', (myLibrary.length - 1));
     div.textContent = `Title: ${book.title}   Author: ${book.author}    Pages: ${book.pages}   Read: ${book.yes}`;
+    removeButton = document.createElement("button");
+    removeButton.classList.add('remove-button');
+    removeButton.setAttribute('data-index', (myLibrary.length - 1));
+    removeButton.addEventListener('click', () => {
+        // removeBook(div.dataset.index);
+        div.remove();
+        //need to also delete that book from the array
+        myLibrary.splice(div.dataset.index, 1);
+    });
+    div.appendChild(removeButton);
     bookContainer.appendChild(div);
 }
 
+//remove book
+
+// function removeBook (index) {
+//     bookContainer.removeChild(index)
+// }
+
+//Popup form
+
+const newBookButton = document.querySelector('#new-book');
+newBookButton.addEventListener('click', () => {
+    openTheForm();
+});
+
+const submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', () => {
+    addBookToLibrary();
+});
+
+const cancelButton = document.querySelector('#cancel-button');
+cancelButton.addEventListener('click', () => {
+    closeTheForm();
+});
+
+function openTheForm() {
+    document.getElementById("pop-up-form").style.display = "grid";
+}
+  
+function closeTheForm() {
+    document.getElementById("pop-up-form").style.display = "none";
+}
+
+
+// window.onclick = function (event) {
+//     let modal = document.getElementById('form-wrapper');
+//     if (event.target == modal) {
+//       closeForm();
+//       alert('its working');
+//     }
+// }
