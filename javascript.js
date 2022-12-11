e95c6635a7790285e2e0f38389a12cedb35ec7c2
Aaron <stephenaaroneaves@gmail.com>
2022-12-11 02:33:00 -0500

    Beginnings of Library is set up   New book is created and added to array   Old divs (i.e. books) are deleted   New divs are displayed via (blank) "cards"

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const yes = document.querySelector('#yes');
 const no = document.querySelector('#no');
+const bookContainer = document.querySelector('.book-container');
 
 const submitButton = document.querySelector('#submit-button');
 submitButton.addEventListener('click', () => {
@@ -20,10 +21,25 @@ function Book(title, author, pages, read ) {
 
 function addBookToLibrary() {
     window.book = new Book(title.value, author.value, pages.value, yes.checked);
+    myLibrary.push(book);
+    while (bookContainer.firstChild) {
+        bookContainer.removeChild(bookContainer.firstChild);
+    }
+    myLibrary.forEach(element => createCard(element));
     title.value = null;
     author.value = null;
     pages.value = null;
     yes.checked = false;
     no.checked = false;
+}
+
+function createCard () {
+    div = document.createElement("div");
+    div.classList.add('box');
+    div.style.height = "200px";
+    div.style.width = "200px";
+    div.style.backgroundColor = '#c0c0c0';
+    div.style.border = "solid 1px #000000";
+    bookContainer.appendChild(div);
 }
 
