ceb09491c99b7f19518f5c36ffd97b7046ff1c89
Aaron <stephenaaroneaves@gmail.com>
2022-12-18 02:47:13 -0500

    Final edits   Changed styling for meta-data divs   New Book button is now inoperable if is form open   Deleted unused images   Cleaned up and organized html, css, and js docs

--- javascript.js
+++ javascript.js
@@ -1,8 +1,6 @@
-//Library, Book, and books
+//Constructor, books, library, and meta-data
 
 let myLibrary = [];
-
-let readBook;
 
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
@@ -10,25 +8,6 @@ const pages = document.querySelector('#pages');
 const yes = document.querySelector('#yes');
 const no = document.querySelector('#no');
 const bookContainer = document.querySelector('.book-container');
-
-const total = document.querySelector('.meta-total');
-const read = document.querySelector('.meta-read');
-const notRead = document.querySelector('.meta-not-read');
-function updateMetaData() {
-    total.textContent = 'Total Books: ' +  myLibrary.length;
-    readBook = 0;
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].read) {
-            readBook += 1;
-        }
-    }
-    read.textContent = 'Read: ' + readBook;
-    notRead.textContent = 'Not Read: ' + (myLibrary.length - readBook);
-}
-
-
-
-
 
 function Book(title, author, pages, read ) {
     this.title = title;
@@ -111,13 +90,47 @@ function createCard () {
     bookContainer.appendChild(card);
 };
 
-//Popup form
+let readBook;
+
+const total = document.querySelector('.meta-total');
+const read = document.querySelector('.meta-read');
+const notRead = document.querySelector('.meta-not-read');
+
+function updateMetaData() {
+    total.textContent = myLibrary.length;
+    readBook = 0;
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].read) {
+            readBook += 1;
+        }
+    }
+    read.textContent = readBook;
+    notRead.textContent = myLibrary.length - readBook;
+}
+
+updateMetaData();
+
+//Form
 
 const newBookButton = document.querySelector('#new-book');
+const popUpForm = document.querySelector('#pop-up-form');
 newBookButton.addEventListener('click', () => {
-    openTheForm();
-    title.focus();
+    if (popUpForm.style.display = "grid") {
+        return
+    } else {
+        popUpForm.style.display = "grid";
+        title.focus();
+    }
 });
+
+function closeTheForm() {
+    document.getElementById("pop-up-form").style.display = "none";
+    title.value = null;
+    author.value = null;
+    pages.value = null;
+    yes.checked = false;
+    no.checked = false;
+}
 
 const removeAll = document.querySelector('#remove-all');
 removeAll.addEventListener('click', () => {
@@ -133,9 +146,7 @@ removeAll.addEventListener('click', () => {
 });
 
 const submitButton = document.querySelector('#submit-button');
-submitButton.addEventListener('click', (event) => {
-    //constrain the pages here
-    
+submitButton.addEventListener('click', (event) => {  
     if (title.value && author.value && pages.value && (yes.checked || no.checked)) {
         if (pages.value > 0 && pages.value < 10000 && (pages.value % 1 == 0 )) {
             addBookToLibrary();
@@ -153,17 +164,3 @@ const cancelButton = document.querySelector('#cancel-button');
 cancelButton.addEventListener('click', () => {
     closeTheForm();
 });
-
-const popUpForm = document.querySelector('#pop-up-form');
-function openTheForm() {
-    popUpForm.style.display = "grid";
-}
-  
-function closeTheForm() {
-    document.getElementById("pop-up-form").style.display = "none";
-    title.value = null;
-    author.value = null;
-    pages.value = null;
-    yes.checked = false;
-    no.checked = false;
-}
